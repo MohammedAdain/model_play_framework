3e26ccde8bf8bcd7e712c29cfa3d4c3b0192e01c
Mohammed Wazir Adain <mwadain@gmail.com>
2025-12-19 14:21:40 +0530

    simple api request

---
+++ role_play_framework.py
@@ -0,0 +1,20 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.getenv("OPEN_ROUTER_API_KEY"),
+    # base_url="https://api.deepseek.com"
+    base_url="https://openrouter.ai/api/v1"
+)
+
+response = client.chat.completions.create(
+    model="deepseek/deepseek-v3.2",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
